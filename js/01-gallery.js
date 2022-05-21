13cea2c82fb3f072f21bdda33821dcf17fd9e3a3
ЮРІЙ <v16m06a@gmail.com>
2022-05-22 01:55:02 +0300

    1 пункт задания

---
+++ js/01-gallery.js
@@ -0,0 +1,50 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("afterbegin", galleryMarkup);
+
+galleryContainer.addEventListener("click", onGalleryItemClick);
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems
+    .map(({ preview, original, description }) =>{
+        return `
+        <div class ="gallery__item">
+        <a class = "gallery__link" href="${original}">
+        <img 
+        class="gallery__image"
+        src="${preview}" 
+        data-source="${original}"
+        alt="${description}"
+        />
+        </a>
+        </div>
+        `
+    })
+    .join("");
+}
+
+function onGalleryItemClick(e){
+    e.preventDefault();
+
+    if(!e.target.classList.contains("gallery__image")){
+return;
+    } else {
+        const instance = basicLightbox.create(`<img src="${e.target.dataset.source}"/>`,{onShow:() => {
+            {window.addEventListener('keydown', onEscPress)}
+        }
+    });
+
+        instance.show();
+
+        function onEscPress(e) {
+            if (e.code === "Escape") {
+                instance.close();
+                window.removeEventListener("keydown", onEscPress);
+            }
+    }
+    }
+}
